ad1a80c88d094d65dc63369e5a6d61e2c8ff181f
novboi <76882455+novboi@users.noreply.github.com>
2021-05-24 15:47:26 +0530

    Create Program for printing positive numbers.py

---
+++ Program for printing positive numbers.py
@@ -0,0 +1,12 @@
+list1=12,-7,5,64,-14
+list2=12,14,-95,3
+print("here is the outputfor list 1:")
+for num in list1:
+    if num>=0:
+        print(num,end=" ")
+print("\nhere is the outputfor list 2:")        
+for num in list2:
+    if num>=0:
+        print(num,end=" ")
+        
+#  I also tried this program with def,but as it makes it as function it returns "None", which we don't 😅want    
